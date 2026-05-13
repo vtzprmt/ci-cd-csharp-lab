8832e94cf6dcd7001595a2c69fc028ebc2878ee7
Клим <hndbk@bk.ru>
2026-05-14 00:21:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.Add(2, 3));
+        }
+    }
+}
